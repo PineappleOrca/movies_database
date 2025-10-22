aa22ea8b69ed40e7c1a501875c9f4d572b2449c1
Sophia Singh <sssingh575@gmail.com>
2025-10-22 11:17:13 -0400

    Update add_movie.py
    add movie comments

--- pages/add_movie.py
+++ pages/add_movie.py
@@ -6,8 +6,8 @@ st.title("Add a New Movie or Series or Book")
 title = st.text_input("Movie Title")
 type_of_content = st.radio("Type of Content", ["Movie", "Series", "Book"])
 if type_of_content == "Movie":
-    genre_options = ["Horror", "Animation", "Other"]
-elif type_of_content == "Series":
+    genre_options = ["Horror", "Animation", "Other"] # could make this part of your .db or have a json file which reads in this information. Can demonstrate file parsing and allows you to construct the map. 
+elif type_of_content == "Series": # switch statement? 
     genre_options = ["Anime", "Other"]
 else:
     genre_options = ["Thriller", "Mystery", "Romance"]
@@ -17,7 +17,7 @@ date = st.text_area("Date Watched", "No Data")
 
 if st.button("Submit"):
     if title.strip() == "":
-        st.error("⚠️ Title cannot be empty!")
+        st.error("⚠️ Title cannot be empty!") # catch and log error? 
     else:
         add_movie(title, type_of_content, genre)
-        st.success(f"Movie '{title}' added successfully!")
+        st.success(f"Movie '{title}' added successfully!") 
