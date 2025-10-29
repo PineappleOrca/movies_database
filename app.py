74fdb92a635652e0ed48101f19bca439b71a5a1f
PineappleOrca <kevinksingh05@gmail.com>
2025-10-29 18:57:20 +0000

    added function to get_stats to return the most watched movie, updated app.py to display on main screen

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from db import create_table, create_wish_list_table, get_last_movie
+from utils.get_stats import get_most_watched_movie
 
 st.set_page_config(page_title="Movie Tracker", layout="wide")
 
@@ -9,9 +10,11 @@ st.set_page_config(page_title="Movie Tracker", layout="wide")
 
 # Storing some data in Variables which will print on the main screen
 last_movie_watched = get_last_movie()
+most_watched_movie = get_most_watched_movie()
 
 # Main streamlit print to screen code
 st.title("🎬📚 Movies, Series, Anime & Books Tracker")
 st.markdown("Use the sidebar to navigate between pages.")
 st.markdown("Track what you’ve watched and what you plan to watch together 💑")
 st.write(f"Last Movie Watched: {last_movie_watched}")
+st.write(f"Most Frequentle Watched Movie: {most_watched_movie}")
--- utils/get_stats.py
+++ utils/get_stats.py
@@ -1,4 +1,29 @@
 import pandas as pd 
+import os
+import sqlite3
 # This is a repository of helper functions for statistics to be displayed
+
+# Global variables to locate the databases
+DB_FOLDER = "database"
+DB_NAME = os.path.join(DB_FOLDER, "movies.db")
+
 def get_sum(df):
     return df['times_watched'].sum()
+
+def get_most_watched_movie()->str:
+    '''
+    This function when called returns the most frequently watched movie name as a string
+    '''
+    conn = sqlite3.connect(DB_NAME)
+    query = """
+    SELECT title
+    FROM movies
+    ORDER BY times_watched DESC
+    LIMIT 1;
+    """
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    last_title = ""
+    if not df.empty:
+        last_title = df.iloc[0]['title']
+    return last_title
