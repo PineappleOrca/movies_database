66a8fc7b9f8170b877046353c0a966b02f1bffa2
PineappleOrca <kevinksingh05@gmail.com>
2025-12-13 10:28:27 +0000

    improved the functionality of the update_content_watch_status function

--- utils/update_dataframes.py
+++ utils/update_dataframes.py
@@ -2,26 +2,42 @@ import pandas as pd
 import os 
 import sqlite3 
 
-def read_database()->pd.DataFrame:
+def get_database():
+    DB_FOLDER = "database"
+    DB_NAME = os.path.join(DB_FOLDER, "movies.db")
+    return sqlite3.connect(DB_NAME)    
+
+def fetch_database()->pd.DataFrame:
     """
     Docstring for read_database
     :params: None
     :return: This is a general function which we can use to read in the entire movies database
     :rtype: pandas DataFrame
     """
-    DB_FOLDER = "database"
-    DB_NAME = os.path.join(DB_FOLDER, "movies.db")
-    conn = sqlite3.connect(DB_NAME)
+    conn = get_database()
     query = """
     SELECT * FROM MOVIES;
     """
     return pd.read_sql_query(query, conn)
 
-def update_content_watch_status(name: str, )-> None:
+def update_content_watch_status(content_name: str, watch_status: str)-> None:
     """
-    Docstring for update_content_status
+    This function takes in two strings , one content name and one watch status and updates the movies.db database depending ont he inputs
     
-    :param df: Takes in a Pandas df
+    :params: content_name, watch_status (str)
     :returns: None
     """
+    query = """
+    UPDATE movies
+    SET watch_status = ?
+    WHERE title = ?
+    """
+    conn = get_database()
+    try:
+        conn.execute(query, (watch_status, content_name))
+    except sqlite3.Error as e:
+        print(f"An error has occured {e}")
+    finally:
+        # Closing the connection to the database
+        conn.close()
 
