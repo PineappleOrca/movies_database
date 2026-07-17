873c6913cd1113e1d300861dc6bc29132c279a81
PineappleOrca <kevinksingh05@gmail.com>
2026-07-17 16:54:54 +0100

    added in boilerplate for fastapi implementation and folder structure, added front end folder as well

---
+++ backend/api/app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.routes import movies#, wishlist, stats
+import uvicorn
+
+app = FastAPI(title="Movies Database API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["localhost:8501"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include routers (this is where APIRouter gets registered)
+app.include_router(movies.router)
+#app.include_router(wishlist.router)
+#app.include_router(stats.router)
+
+@app.get("/")
+def root():
+    return {"message": "Movies Database API"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
